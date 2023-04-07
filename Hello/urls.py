b9e17fe6ce1004dc97ab5742585b2808d9c0146a
Aditya Mogha <moghaaditya@gmail.com>
2023-04-07 16:26:34 +0530

    admin

--- Hello/urls.py
+++ Hello/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Apex Portal"
+admin.site.site_title = "Apex Portal"
+admin.site.index_title = "Welcome to Apex Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Home.urls'))
